6d17951438cc0cf925a061f0d80e7c90802ebb52
piotrekpuszczynski <piotrekpuszcz@icloud.com>
2021-05-21 14:58:48 +0200

    deleting database from main activity

--- lista5/app/src/main/java/com/example/pong/MainActivity.kt
+++ lista5/app/src/main/java/com/example/pong/MainActivity.kt
@@ -2,35 +2,72 @@ package com.example.pong
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import androidx.room.Room
-import com.example.pong.database.AppDatabase
-import com.example.pong.database.Player
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.lang.Exception
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val CODE = 100
-    }
+    //private lateinit var database : AppDatabase
 
-    private lateinit var database : AppDatabase
+    companion object {
+        const val RC_SIGN_IN = 10
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
         GlobalScope.launch {
             try {
                 database = Room.databaseBuilder(
-                    applicationContext,
-                    AppDatabase::class.java,
-                    "players.db"
+                        applicationContext,
+                        AppDatabase::class.java,
+                        "players.db"
                 ).build()
+                database.playerDao().insertPlayer(Player("player one"))
+                database.playerDao().insertPlayer(Player("player two"))
+
             } catch (e: Exception) {}
+        } */
+
+        val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build())
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            //val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                //val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this,
+                        "Successfully signed in. Welcome!",
+                        Toast.LENGTH_LONG)
+                        .show()
+            } else {
+                Toast.makeText(this,
+                        "We couldn't sign you in. Please try again later.",
+                        Toast.LENGTH_LONG)
+                        .show()
+
+                finish()
+            }
         }
     }
 
@@ -38,10 +75,19 @@ class MainActivity : AppCompatActivity() {
         when(view.id) {
             R.id.singleplayer -> {
                 val intent = Intent(this, GameActivity::class.java)
-                startActivityForResult(intent, CODE)
+                startActivity(intent)
             } R.id.multiplayer -> {
+            val user = FirebaseAuth.getInstance().currentUser
 
-            } else -> {
+            val users : ArrayList<String> = ArrayList()
+            val rootRef = FirebaseDatabase.getInstance().reference
+
+            val usersdRef = rootRef.child("users")
+
+            Log.i("sss", usersdRef.toString())
+
+
+        } else -> {
                 val intent = Intent(this, StatisticsActivity()::class.java)
                 startActivity(intent)
             }
@@ -49,17 +95,12 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CODE && resultCode == Activity.RESULT_OK) {
-            GlobalScope.launch {
-                database.playerDao().updatePoints("player one",
-                    data?.getStringExtra("playerOnePoints")!!.toInt())
-                database.playerDao().updatePoints("player two",
-                    data.getStringExtra("playerTwoPoints")!!.toInt())
-                database.playerDao().updateWinner(data.getStringExtra("won")!!, 1)
-            }
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener {
+                    // ...
+                }
     }
-
 }
